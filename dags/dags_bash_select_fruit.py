7fdbcef0faffdc3c820894d64c1d2d1822164c17
foreveryounge <marcowyoung@gmail.com>
2025-02-09 14:50:34 +0900

    select_fruit.sh 수행하는 DAG 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -12,12 +12,12 @@ with DAG(
 
     t1_orange = BashOperator(
         task_id="t1_orange",
-        bash_command="opt/airflow/plugins/shell/select_fruit.sh ORANGE",  # Task를 실행하는 주체는 Woker 컨테이너
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",  # Task를 실행하는 주체는 Woker 컨테이너
     )
 
     t2_avocado = BashOperator(
         task_id="t1_avocado",
-        bash_command="opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
     t1_orange >> t2_avocado
