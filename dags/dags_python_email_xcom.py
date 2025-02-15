f247732ae672df47d8ded0099a8e0b432c1ec0d9
foreveryounge <marcowyoung@gmail.com>
2025-02-15 23:18:59 +0900

    python_email_xcom dag 예제 파일 업로드

---
+++ dags/dags_python_email_xcom.py
@@ -0,0 +1,27 @@
+import pendulum
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id="dags_python_email.xcom",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    @task(task_id="something_task")
+    def some_logic(**kwargs):
+        from random import choice
+
+        return choice(["Sucess", "Fail"])
+
+    send_email = EmailOperator(
+        task_id="send_email",
+        to="durmkim@naver.com",
+        subject="{{data_interval_end.in_timezone('Asia/Seoul') | ds}} Some_logic 처리 결과",
+        html_content="{{data_interval_end.in_timezone('Asia/Seoul') | ds}} 처리 결과는 <br> \
+                    {{ti.xcom_pull(task_ids='something_task')}} 했습니다. <br>",
+    )
+
+    some_logic() >> send_email
