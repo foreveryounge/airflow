c26caded6195ef5eaf7f0c9e840f2b4f300d1527
foreveryounge <marcowyoung@gmail.com>
2025-02-15 09:04:48 +0900

    python template dag 업로드

---
+++ dags/dags_python_show_template.py
@@ -0,0 +1,20 @@
+import pendulum
+import datetime
+from pprint import pprint
+from airflow import DAG
+from airflow.decorators import task
+
+tz = pendulum.timezone("Asia/Seoul")
+
+with DAG(
+    dag_id="dags_python_show_template",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 2, 10, tz=tz),
+    catchup=True,
+) as dag:
+
+    @task(taks_id="python_task")
+    def show_template(**kwargs):
+        pprint(kwargs)
+
+    show_template()
