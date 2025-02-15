8e3b13a7308f51491cb66fa720c2e08b6a1d95fa
foreveryounge <marcowyoung@gmail.com>
2025-02-15 13:44:43 +0900

    python_xcom dag 예제 업로드

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -5,7 +5,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_python_with_xcom_eg2",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2025, 2, 1, tz="Asia/seoul"),
+    start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
 
