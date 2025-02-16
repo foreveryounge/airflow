c21b7f0223214c66ae88a013a4eb90936840ea30
foreveryounge <marcowyoung@gmail.com>
2025-02-16 15:27:36 +0900

    seoul_api_corona dag 예제 파일 업로드

---
+++ dags/dags_seoul_api_corona.py
@@ -0,0 +1,28 @@
+import pendulum
+from airflow import DAG
+from operators.seoul_api_to_csv_operators import SeoulApiToCsvOperator
+
+with DAG(
+    dag_id="dags_seoul_api_corona",
+    schedule="0 7 * * *",
+    start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    """서울시 공공자전거 이용정보(일별)"""
+    tb_cycle_rent_use_dayinfo = SeoulApiToCsvOperator(
+        task_id="tb_cycle_rent_use_dayinfo",
+        dataset_nm="tbCycleRentUseDayInfo",
+        path="/opt/airflow/files/tbCycleRentUseDayInfo/{{data_interval_end.in_timezone('Asia/Seoul') | ds_nodash}}",
+        file_name="tbCycleRentUseDayInfo.csv",
+    )
+
+    """서울시 공공자전거 고장신고 내역"""
+    tb_cycle_failure_report = SeoulApiToCsvOperator(
+        task_id="tb_cycle_failure_report",
+        dataset_nm="tbCycleFailureReport",
+        path="/opt/airflow/files/tbCycleFailureReport/{{data_interval_end.in_timezone('Asia/Seoul') | ds_nodash}}",
+        file_name="tbCycleFailureReport.csv",
+    )
+
+    tb_cycle_rent_use_dayinfo >> tb_cycle_failure_report
