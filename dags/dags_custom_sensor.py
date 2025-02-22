e65b2129da01a32ecff9ae3778efb7c3edec37fb
foreveryounge <marcowyoung@gmail.com>
2025-02-23 08:19:26 +0900

    custom_sensor dag 파일 업로드

---
+++ dags/dags_custom_sensor.py
@@ -0,0 +1,28 @@
+import pendulum
+from airflow import DAG
+from sensors.seoul_api_date_sensor import SeoulApiDateSensor
+
+with DAG(
+    dag_id="dags_custom_sensor",
+    schedule=None,
+    start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    tb_cycle_rent_use_dayinfo_sensor = SeoulApiDateSensor(
+        task_id="tb_cycle_rent_use_dayinfo_sensor",
+        dataset_nm="tbCycleRentUseDayInfo",
+        base_dt_col="RENT_DT",
+        day_off=-1,
+        poke_interval=600,
+        mode="reschedule",
+    )
+
+    tb_cycle_failure_report_sensor = SeoulApiDateSensor(
+        task_id="tb_cycle_failure_report_sensor",
+        dataset_nm="tbCycleFailureReport",
+        base_dt_col="REGDTTM",
+        day_off=-1,
+        poke_interval=600,
+        mode="reschedule",
+    )
