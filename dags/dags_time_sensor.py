664565113bc46a5fb102bf1828383a9234337fa7
foreveryounge <marcowyoung@gmail.com>
2025-02-23 14:52:29 +0900

    time_sensor dag 파일 업로드

---
+++ dags/dags_time_sensor.py
@@ -0,0 +1,17 @@
+import pendulum
+from airflow import DAG
+from airflow.sensors.date_time import DateTimeSensor
+
+with DAG(
+    dag_id="dags_time_sensor",
+    schedule="*/10 * * * *",
+    start_date=pendulum.datetime(2025, 2, 1, 0, 0, 0),
+    end_date=pendulum.datetime(2025, 2, 1, 1, 0, 0),
+    catchup=True,
+) as dag:
+
+    sync_sensor = DateTimeSensor(
+        # 설정한 목표 시간까지 기다리는 Sensor
+        task_id="sync_sensor",
+        target_time="""{{ macros.datetime.utcnow() + macros.timedelta(minutes=5) }}""",
+    )
